3b437b53e2ec6c511bba41f84515108ab10d3090
dev <wanjikusophia3@gmail.com>
2025-03-27 10:49:16 +0300

    remove real-time-notifications-system

--- real-time-notifications-system/src/main/java/com/microservice_jwt/real_time_notifications_system/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.microservice_jwt.real_time_notifications_system.repository;
-
-import com.microservice_jwt.real_time_notifications_system.model.Notification;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface NotificationRepository extends MongoRepository<Notification, String> {
-}
